ce5077ae9dcc6191112efada476ddb5763c1a3c1
gouthami-31 <takur.gouthami@s.amity.edu>
2026-06-30 19:00:26 +0530

    i added 2nd code

---
+++ loop.java
@@ -0,0 +1,25 @@
+// File Name: Loop.java
+
+public class Loop {
+    public static void main(String[] args) {
+
+        System.out.println("For Loop:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("\nWhile Loop:");
+        int j = 1;
+        while (j <= 5) {
+            System.out.println(j);
+            j++;
+        }
+
+        System.out.println("\nDo-While Loop:");
+        int k = 1;
+        do {
+            System.out.println(k);
+            k++;
+        } while (k <= 5);
+    }
+}
