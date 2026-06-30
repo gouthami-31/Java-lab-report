677643345d12ebbd874f51cfcbe432eb9c0e8684
gouthami-31 <takur.gouthami@s.amity.edu>
2026-06-30 19:25:39 +0530

    12th code

---
+++ Armstrong.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int temp = n, sum = 0;
+
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += digit * digit * digit;
+            temp /= 10;
+        }
+
+        if (sum == n)
+            System.out.println("Armstrong Number");
+        else
+            System.out.println("Not Armstrong Number");
+
+        sc.close();
+    }
+}
