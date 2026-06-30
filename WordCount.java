4fc173f7d1d1770c3d8168fc94d819632ad9efc8
gouthami-31 <takur.gouthami@s.amity.edu>
2026-06-30 21:32:20 +0530

    34 code

---
+++ WordCount.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class WordCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String str = sc.nextLine();
+
+        String[] words = str.trim().split("\\s+");
+
+        System.out.println("Total Words = " + words.length);
+
+        sc.close();
+    }
+}
