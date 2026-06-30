3442a2e77dc71abeddd4ef982407ff8f20fecfb7
gouthami-31 <takur.gouthami@s.amity.edu>
2026-06-30 21:40:02 +0530

    38 code

---
+++ LinkedListDemo.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.println(list);
+
+        list.removeFirst();
+
+        System.out.println(list);
+    }
+}
