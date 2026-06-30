e68f0a7482e03b359d494c55a920aea14a93ba9e
gouthami-31 <takur.gouthami@s.amity.edu>
2026-06-30 21:37:38 +0530

    37 code

---
+++ ArrayListDemo.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Orange");
+
+        System.out.println("ArrayList:");
+
+        for (String fruit : list)
+            System.out.println(fruit);
+    }
+}
